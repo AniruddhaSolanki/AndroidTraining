6b630fcf4525490e4e9cd1129e44abd34c2441ec
sraji23 <ssolanki4256@gmail.com>
2019-12-30 18:33:50 +0530

    Second

---
+++ AT12.java
@@ -0,0 +1,18 @@
+
+
+public class variable {
+    public static void StudentAge()
+    {
+        // local variable age
+        int age = 0;
+        age = age + 5;
+        System.out.println("Student age is : " + age);
+    }
+
+    public static void main(String args[])
+    {
+
+        StudentAge();
+       // System.out.println("Student age is : " + age);  if we access the local variable from outside the block of function student age
+    }
+}
